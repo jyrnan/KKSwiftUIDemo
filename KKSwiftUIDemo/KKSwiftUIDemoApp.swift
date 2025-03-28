64a2e01196f584c9ed184787005404858840964b
jyrnan <yoeking@gmail.com>
2025-03-28 15:57:07 +0800

    Initial Commit

---
+++ KKSwiftUIDemo/KKSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KKSwiftUIDemoApp.swift
+//  KKSwiftUIDemo
+//
+//  Created by jyrnan on 2025/3/28.
+//
+
+import SwiftUI
+
+@main
+struct KKSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
